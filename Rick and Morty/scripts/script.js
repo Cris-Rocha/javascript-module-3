54f8bb0c5bf1703369be4e7ba2c9e3eeaace411c
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-04-18 22:18:30 +0200

    adding the function that makes easier the creation of HTML elements

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -1,64 +1,66 @@
+const root = document.getElementById("root");
 
-const root = document.getElementById('root');
+//function that Creates the elements on HTML
 
-// encabezado
-const headerSection = document.createElement('div'); // con comillas
-headerSection.id = "header";
-root.appendChild(headerSection) // aqui sin comillas
+const htmlElementCreator = () => {
+  const buildRoot = document.getElementById("root");
+  root.innerHTML =
+    '<div id="header"></div>' +
+    '<div id="content">' +
+    '<div id="sidebar"><div id="chapters"></div></div>' +
+    '<div id="main"></div>'
+    '</div>';
+};
 
-headerSection.innerHTML = "<h1>Rick and Morty API</h1>"
+htmlElementCreator()
 
-//menu de episodios
-const sideabarSection = document.createElement('sidebar')
-sideabarSection.id = "sidebar-menu";
-root.appendChild(sideabarSection)
+// es lo mismo que hacer asi, pero para cada uno de los elementos: 
 
-// contenedor con las fotos
-const contenedorImages = document.createElement('container');
-contenedorImages.id = "container-img";
-root.appendChild(contenedorImages)
+// const headerSection = document.createElement("div"); // con comillas
+// headerSection.id = "header";
+// root.appendChild(headerSection); // aqui sin comillas
 
+// // headerSection.innerHTML = "<h1>Rick and Morty API</h1>"
 
+// //menu de episodios
+// const sideabarSection = document.createElement("sidebar");
+// sideabarSection.id = "sidebar-menu";
+// root.appendChild(sideabarSection);
+
+// // contenedor con las fotos
+// const contenedorImages = document.createElement("container");
+// contenedorImages.id = "container-img";
+// root.appendChild(contenedorImages);
 
 function loadEpisodes() {
-    fetch("https://rickandmortyapi.com/api/episode")
-    .then(res => res.json())
-    .then(episodes => showEpisodeLinks(episodes));
+  fetch("https://rickandmortyapi.com/api/episode")
+    .then((res) => res.json())
+    .then((episodes) => showEpisodeLinks(episodes));
 }
 
-function showEpisodeLinks(episodes){
-    
-    episodes.results
-    .map(episode =>createEpisodeLink(episode))
-    .forEach(episode => console.log(episode.name))
-
+function showEpisodeLinks(episodes) {
+  episodes.results
+    .map((episode) => createEpisodeLink(episode))
+    .forEach((episode) => console.log(episode.name));
 }
 
-function createEpisodeLink(episode){
-    const episodeLink = document.createElement("div")
-    episodeLink.classList.add('episodeLink')
-    episodeLink.innerText = episode.name
-    episodeLink.addEventListener('click', () => showEpisodeDetail(episode))
-    return episodeLink
+function createEpisodeLink(episode) {
+  const episodeLink = document.createElement("div");
+  episodeLink.classList.add("episodeLink");
+  episodeLink.innerText = episode.name;
+  episodeLink.addEventListener("click", () => showEpisodeDetail(episode));
+  return episodeLink;
 }
 
-function showEpisodeDetail(episode){
-   const main = document.getElementById('main')
-   main.innerText = `<h2>${episode.name}</h2>` + `<h3>${episode.episode} | ${episode.air_date}</h3>`
+function showEpisodeDetail(episode) {
+  const main = document.getElementById("main");
+  main.innerText =
+    `<h2>${episode.name}</h2>` +
+    `<h3>${episode.episode} | ${episode.air_date}</h3>`;
 
-   episode.character.forEach(characterUrl => console.log(characterUrl))
-       
-   };
-
-function createCharacterThumbnail(characterUrl) {
-
-}   
-
-function renderCharacterThumbnail (){
-    
+  episode.character.forEach((characterUrl) => console.log(characterUrl));
 }
 
+function createCharacterThumbnail(characterUrl) {}
 
-
-
-
+function renderCharacterThumbnail() {}
