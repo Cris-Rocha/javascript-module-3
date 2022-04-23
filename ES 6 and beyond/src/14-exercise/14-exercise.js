24ecab12d7d736b4d9ed79077ebe04fb14ccb58c
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-04-23 17:36:28 +0200

    adding the exercise 14 of the inclass

--- ES 6 and beyond/src/14-exercise/14-exercise.js
+++ ES 6 and beyond/src/14-exercise/14-exercise.js
@@ -12,3 +12,24 @@
  *    return "something";
  * };
  */
+
+ function add (num1, num2) {
+    return num1 +num2
+}
+
+export {add}
+
+/* resultado del test:
+
+ PASS  src/__tests__/14-exercise.test.js
+  14-exercise
+    ✓ export a named function that returns the sum of it’s parameters (47 ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       1 passed, 1 total
+Snapshots:   0 total
+Time:        0.91 s, estimated 1 s
+
+
+
+*/
