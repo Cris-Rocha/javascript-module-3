909fbb3b6a8b89218bc32b1d2a842c7575d2c9a9
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-04-23 17:17:05 +0200

    adding exercise 11 of the homework

--- ES 6 and beyond/src/11-exercise/11-exercise.js
+++ ES 6 and beyond/src/11-exercise/11-exercise.js
@@ -16,7 +16,23 @@
  */
 
 // Finish the code of the function
-function exercise11() {}
+function exercise11(array, entry) {
+    return array.find((elemento) => elemento == entry)
+}
 
 // Don’t change the code bellow this line
 export default exercise11;
+
+/* resultado del test:
+
+ PASS  src/__tests__/11-exercise.test.js
+  11-exercise
+    ✓ use the array.find() method to find in the array the value of the parameter (1 ms)
+    ✓ return the found entry in the array
+
+Test Suites: 1 passed, 1 total
+Tests:       2 passed, 2 total
+Snapshots:   0 total
+Time:        0.926 s, estimated 1 s
+Ran all test suites matching /src\/__tests__\/11-exercise.test.js/i.
+*/
