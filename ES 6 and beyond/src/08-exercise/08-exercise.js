386de1bbc59ccebed105cd1a49da76142875fb32
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-04-23 16:07:03 +0200

    adding exercise 8 of  homework

--- ES 6 and beyond/src/08-exercise/08-exercise.js
+++ ES 6 and beyond/src/08-exercise/08-exercise.js
@@ -10,8 +10,8 @@
  */
 
 // Finish the code of the function
-function exercise08() {
-    function func(a, b) {
+function exercise08 () {
+    let func = (a, b) => {
         return a + b;
     }
 
@@ -21,3 +21,21 @@ function exercise08() {
 
 // Don’t change the code bellow this line
 export default exercise08;
+
+
+/* prints:
+
+ PASS  src/__tests__/08-exercise.test.js
+  08-exercise
+    ✓ return the `func` function (2 ms)
+    ✓ func should be an arrow function (1 ms)
+    ✓ func should return the sum of the parameters (1 ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       3 passed, 3 total
+Snapshots:   0 total
+Time:        0.924 s
+Ran all test suites matching /src\/__tests__\/08-exercise.test.js/i.
+occ-0067@pc-189 ES 6 and beyond % 
+
+*/
