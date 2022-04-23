79897883862a94ba8d2a2c81af79698a5ae67239
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-04-23 17:40:50 +0200

    adding the exercise 15 of the inclass

--- ES 6 and beyond/src/14-exercise/14-exercise.js
+++ ES 6 and beyond/src/14-exercise/14-exercise.js
@@ -14,7 +14,7 @@
  */
 
  function add (num1, num2) {
-    return num1 +num2
+    return num1 + num2
 }
 
 export {add}
--- ES 6 and beyond/src/15-exercise/15-exercise.js
+++ ES 6 and beyond/src/15-exercise/15-exercise.js
@@ -14,3 +14,23 @@
  *
  * export default test;
  */
+
+
+ function divide (num1, num2) {
+    return num1 /num2
+}
+
+export default divide
+
+/* resultado del test:
+
+ PASS  src/__tests__/15-exercise.test.js
+  15-exercise
+    ✓ default export a function that returns the division of it’s parameters (56 ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       1 passed, 1 total
+Snapshots:   0 total
+Time:        0.927 s, estimated 1 s
+
+*/
