137275d747bff36042cafdf3e1e517f9a96a9545
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-04-23 17:55:46 +0200

    adding the exercise 16 of the inclass

--- ES 6 and beyond/src/16-exercise/16-exercise.js
+++ ES 6 and beyond/src/16-exercise/16-exercise.js
@@ -7,3 +7,19 @@
  * You can use the `export * as name from "./url-path"` command
  * to export all the modules in a file under a particular namespace
  */
+
+export *  as utils from './16-exercise-fns'
+
+/* resultado del test :
+
+
+ PASS  src/__tests__/16-exercise.test.js
+  16-exercise
+    ✓ export all the modules under the `utils` namespace (81 ms)
+
+Test Suites: 1 passed, 1 total
+Tests:       1 passed, 1 total
+Snapshots:   0 total
+Time:        0.953 s, estimated 1 s
+
+*/
