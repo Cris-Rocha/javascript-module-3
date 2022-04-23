43ef546e50e8dda9613e962b4a963b67930eb627
Cris-Rocha <cris-t-rocha@hotmail.com>
2022-04-23 17:02:22 +0200

    adding exercise 10 of the homework

--- ES 6 and beyond/src/10-exercise/10-exercise.js
+++ ES 6 and beyond/src/10-exercise/10-exercise.js
@@ -11,7 +11,26 @@
 // Finish the code of the function
 function exercise10() {
     const message = "hello-world";
+    return Array.from(message)
 }
 
 // Don’t change the code bellow this line
 export default exercise10;
+
+
+/* resultado del test:
+
+ PASS  src/__tests__/10-exercise.test.js
+  10-exercise
+    ✓ use the Array.from() method to create an array from the string (2 ms)
+    ✓ return the array of characters
+
+Test Suites: 1 passed, 1 total
+Tests:       2 passed, 2 total
+Snapshots:   0 total
+Time:        0.92 s, estimated 2 s
+Ran all test suites matching /src\/__tests__\/10-exercise.test.js/i.
+occ-0067@pc-189 ES 6 and beyond % 
+
+
+*/
